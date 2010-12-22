1598d9f928588bb33fd311e6445d52730431ab6f
Fabio Kreusch <fabio@kreusch.com.br>
2010-12-22 13:04:40 -0200

    Switched autotest to autotest-rails

--- lib/recipes/tests/rspec.rb
+++ lib/recipes/tests/rspec.rb
@@ -1,6 +1,7 @@
 gem 'rspec-rails', '~> 2.0.1', :group => [:development, :test]
 gem 'spork', '>= 0.9.0.rc2', :group => [:development, :test]
-gem 'autotest', :group => [:development, :test]
+gem 'autotest-rails', :group => [:development, :test]
+gem 'autotest-notification', :group => [:development, :test]
 gem 'steak', '>= 1.0.0.rc.1', :group => [:development, :test]
 gem 'capybara', :group => [:development, :test]
 gem 'launchy', :group => [:development, :test]
@@ -9,7 +10,7 @@ gem 'factory_girl_rails', :group => [:development, :test]
 #Remarkable matchers
 gem 'remarkable', '>=4.0.0.alpha2', :group => [:test]
 gem 'remarkable_activemodel', '>=4.0.0.alpha4', :group => [:test]
-gem 'remarkable_activerecord', '>=4.0.0.alpha2', :group => [:test] 
+gem 'remarkable_activerecord', '>=4.0.0.alpha2', :group => [:test]
 
 after_gems << lambda do
   remove_file 'test'
@@ -20,7 +21,7 @@ after_gems << lambda do
   append_file '.rspec', '--drb'
 
   spec_helper_requires = %w(remarkable/active_model remarkable/active_record factory_girl ).map {|requirement| "require '#{requirement}'"}.join("\n")
-  inject_into_file "spec/spec_helper.rb", spec_helper_requires, :after => "require 'rspec/rails'\n"  
+  inject_into_file "spec/spec_helper.rb", spec_helper_requires, :after => "require 'rspec/rails'\n"
 
   comment 'spec/spec_helper.rb', 'config.fixture_path'
 
