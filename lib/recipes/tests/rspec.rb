e3e0289c069b3e2258617de9b385a618fef81018
Fabio Kreusch <fabio@kreusch.com.br>
2010-12-28 20:41:07 -0200

    Added spork for rails3

--- lib/recipes/tests/rspec.rb
+++ lib/recipes/tests/rspec.rb
@@ -1,8 +1,10 @@
 gem 'rspec-rails', '~> 2.0.1', :group => [:development, :test]
 gem 'steak', :group => [:development, :test]
 gem 'capybara', :group => [:development, :test]
+gem 'spork', '~> 0.9.0.rc2', :group => [:test]
 gem 'factory_girl_rails', :group => [:test]
 gem 'guard-rspec', :group => [:test]
+gem 'guard-spork', :group => [:test]
 gem 'libnotify', :group => [:test]
 gem 'rb-inotify', :group => [:test]
 gem 'metric_fu', :group => [:test]
@@ -16,12 +18,15 @@ gem 'remarkable_activerecord', '~> 4.0.0.alpha2', :group => [:test]
 after_gems << lambda do
   generate 'rspec:install'
   generate 'steak:install'
+  run 'spork --bootstrap'
 
   remove_file 'test'
   remove_file 'autotest'
 
   spec_helper_requires = %w(remarkable/active_model remarkable/active_record factory_girl cover_me).map {|requirement| "require '#{requirement}'"}.join("\n")
   inject_into_file "spec/spec_helper.rb", spec_helper_requires + "\n", :after => "require 'rspec/rails'\n"
+  inject_into_file "spec/spec_helper.rb", "\n  ActiveSupport::Dependencies.clear\n", :after => "config.use_transactional_fixtures = true\n"
+  inject_into_file "config/application.rb", "\n" + template_content('rspec/config/application.rb.spork'), :after => "config.filter_parameters += [:password]\n"
 
   comment 'spec/spec_helper.rb', 'config.fixture_path'
 
@@ -30,6 +35,7 @@ after_gems << lambda do
   create_file 'lib/tasks/metrics.rake', template_content('rspec/lib/tasks/metrics.rake')
 
   append_file '.gitignore', "coverage.data\n"
+  append_file '.rspec', "--drb\n"
 
   repo_commit 'Added Rspec'
 end
